cb22f345c2488705cf0fff97fc6d2ea39ac5c5e7
timrusin <timrusin@gmail.com>
2022-10-21 15:17:13 -0500

    modal created in gallery for focus Photos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,30 @@
 import { useParams } from 'react-router-dom'
+import { useState } from 'react'
 import './Gallery.css'
 import Photos from '../data/Photos'
 
 
 const Gallery = () => {
   let { category } = useParams()
+  const [modal, setModal] = useState(false)
+  const [focusPhoto, setFocusPhoto] = useState('')
+
+  const getPhoto = (url) => {
+    setFocusPhoto(url)
+    setModal(true)
+  }
   
      return (
+     <>
+     <div className={modal ? "modal open" : "modal"}>
+        <img src={focusPhoto} alt="alt placeholder"/>
+        <i class="fa-regular fa-circle-xmark" onClick={()=>setModal(false)}></i>
+     </div>
        <div className="gallery fade">
          {Photos.map((photo) => {
            return (
              photo.category === category && (
-               <div className="pics" key={photo.id}>
+               <div className="pics" key={photo.id} onClick={()=> getPhoto(photo.url)}>
                  <img
                    src={photo.url}
                    alt={photo.alt}
@@ -22,6 +35,7 @@ const Gallery = () => {
            );
          })}
        </div>
+      </>
      );
 }
 
